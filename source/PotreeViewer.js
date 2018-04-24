93596ae8b61bbd66e0c3f74a19ef729a1ae4ad17
Tentone <tentone@outlook.com>
2018-04-24 10:51:05 +0100

    Clean up

--- source/PotreeViewer.js
+++ source/PotreeViewer.js
@@ -9,16 +9,12 @@ class PotreeViewer
 {
 	constructor()
 	{
-		this.fov = 60;
 		this.minNodeSize = 30;
-		this.moveSpeed = 10;
 		this.scaleFactor = 1;
 
+		this.time = 0;
+
 		Potree.pointBudget = 1000000;
-		
-		this.skybox = null;
-		this.clock = new THREE.Clock();
-		this.background = null;
 
 		this.createRenderer();
 
@@ -35,58 +31,11 @@ class PotreeViewer
 
 	dispatchEvent(){}
 
-	setMoveSpeed(value)
-	{
-		this.moveSpeed = value;
-	}
-
-	zoomTo(node, factor, animationDuration = 0)
-	{
-		var view = this.scene.view;
-
-		var camera = this.scene.cameraP.clone();
-		camera.rotation.copy(this.scene.cameraP.rotation);
-		camera.rotation.order = "ZXY";
-		camera.rotation.x = Math.PI / 2 + view.pitch;
-		camera.rotation.z = view.yaw;
-		camera.updateMatrix();
-		camera.updateMatrixWorld();
-		camera.zoomTo(node, factor);
-
-		var bs;
-		if(node.boundingSphere)
-		{
-			bs = node.boundingSphere;
-		}
-		else if(node.geometry && node.geometry.boundingSphere)
-		{
-			bs = node.geometry.boundingSphere;
-		}
-		else
-		{
-			bs = node.boundingBox.getBoundingSphere();
-		}
-		bs = bs.clone().applyMatrix4(node.matrixWorld); 
-
-		var startPosition = view.position.clone();
-		var endPosition = camera.position.clone();
-		var startTarget = view.getPivot();
-		var endTarget = bs.center;
-		var startRadius = view.radius;
-		var endRadius = endPosition.distanceTo(endTarget);
-
-		view.position.copy(endPosition);
-		view.lookAt(endTarget);
-	}
+	setMoveSpeed(value){}
 
 	getBoundingBox(pointclouds)
 	{
 		return this.scene.getBoundingBox(pointclouds);
-	}
-
-	toggleNavigationCube()
-	{
-		this.navigationCube.visible = !this.navigationCube.visible;
 	}
 
 	setCameraMode(mode)
@@ -98,14 +47,6 @@ class PotreeViewer
 			pointcloud.material.useOrthographicCamera = mode == Potree.CameraMode.ORTHOGRAPHIC;
 		}
 	}
-
-	toggleMap()
-	{
-		if(this.mapView)
-		{
-			this.mapView.toggle();
-		}
-	};
 
 	createRenderer()
 	{
@@ -131,112 +72,46 @@ class PotreeViewer
 		gl.bindVertexArray = extVAO.bindVertexArrayOES.bind(extVAO);		
 	}
 
-	update(delta, timestamp)
+	update(delta)
 	{
-		var u = Math.sin(0.0005 * timestamp) * 0.5 - 0.4;
+		this.time += delta;
+
+		var u = Math.sin(0.0005 * this.time) * 0.5 - 0.4;
 		var x = Math.cos(u);
 		var y = Math.sin(u);
 
-		var scene = this.scene;
-		var camera = scene.getActiveCamera();
-		
 		Potree.pointLoadLimit = Potree.pointBudget * 2;
 
 		for(var pointcloud of this.scene.pointclouds)
 		{
-			if(!pointcloud.material._defaultIntensityRangeChanged)
-			{
-				var root = pointcloud.pcoGeometry.root;
-				if(root != null && root.loaded)
-				{
-					var attributes = pointcloud.pcoGeometry.root.geometry.attributes;
-					if(attributes.intensity)
-					{
-						var array = attributes.intensity.array;
-
-						//Chose max value from the 0.75 percentile
-						var ordered = [];
-						for(var j = 0; j < array.length; j++)
-						{
-							ordered.push(array[j]);
-						}
-						ordered.sort();
-						var capIndex = parseInt((ordered.length - 1) * 0.75);
-						var cap = ordered[capIndex];
-
-						if(cap <= 1)
-						{
-							pointcloud.material.intensityRange = [0, 1];
-						}
-						else if(cap <= 256)
-						{
-							pointcloud.material.intensityRange = [0, 255];
-						}
-						else
-						{
-							pointcloud.material.intensityRange = [0, cap];
-						}
-					}
-				}
-			}
-			
 			pointcloud.showBoundingBox = false;
 			pointcloud.generateDEM = false;
 			pointcloud.minimumNodePixelSize = this.minNodeSize;
 		}
 
-		var result = Potree.updatePointClouds(scene.pointclouds, camera, this.renderer);
+		Potree.updatePointClouds(this.scene.pointclouds, this.scene.cameraP, this.renderer);
 
-		if(result.lowestSpacing !== Infinity)
-		{
-			var near = result.lowestSpacing * 10.0;
-			var far = -this.getBoundingBox().applyMatrix4(camera.matrixWorldInverse).min.z;
+		this.controls.setScene(this.scene);
+		this.controls.update(delta);
 
-			far = Math.max(far * 1.5, 1000);
-			near = Math.min(100.0, Math.max(0.01, near));
-			far = Math.max(far, near + 1000);
-
-			if(near === Infinity)
-			{
-				near = 0.1;
-			}
-			
-			camera.near = near;
-			camera.far = far;
-		}
-
-		if(this.scene.cameraMode == Potree.CameraMode.ORTHOGRAPHIC)
-		{
-			camera.near = -camera.far;
-		}
-		
-		this.scene.cameraP.fov = this.fov;
-		
-		if(this.controls !== null)
-		{
-			this.controls.setScene(scene);
-			this.controls.update(delta);
-
-			this.scene.cameraP.position.copy(scene.view.position);
-			this.scene.cameraP.rotation.order = "ZXY";
-			this.scene.cameraP.rotation.x = Math.PI / 2 + this.scene.view.pitch;
-			this.scene.cameraP.rotation.z = this.scene.view.yaw;
-
-			this.scene.cameraO.position.copy(scene.view.position);
-			this.scene.cameraO.rotation.order = "ZXY";
-			this.scene.cameraO.rotation.x = Math.PI / 2 + this.scene.view.pitch;
-			this.scene.cameraO.rotation.z = this.scene.view.yaw;
-		}
-		
-		camera.updateMatrix();
-		camera.updateMatrixWorld();
-		camera.matrixWorldInverse.getInverse(camera.matrixWorld);
+		this.scene.cameraP.fov = 60;
+		this.scene.cameraP.position.copy(this.scene.view.position);
+		this.scene.cameraP.rotation.order = "ZXY";
+		this.scene.cameraP.rotation.x = Math.PI / 2 + this.scene.view.pitch;
+		this.scene.cameraP.rotation.z = this.scene.view.yaw;
+		this.scene.cameraP.updateMatrix();
+		this.scene.cameraP.updateMatrixWorld();
+		this.scene.cameraP.matrixWorldInverse.getInverse(this.scene.cameraP.matrixWorld);
 	}
 	
-	render()
+	render(camera)
 	{
-		var activeCam = this.scene.getActiveCamera();
-		this.pRenderer.render(this.scene.scenePointCloud, activeCam);
+		if(camera === undefined)
+		{
+			camera = this.scene.cameraP;
+		}
+		
+		this.pRenderer.render(this.scene.scenePointCloud, camera);
 	}
 
 	resize()

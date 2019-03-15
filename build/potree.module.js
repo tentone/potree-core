bdb995cd65ef1a3cd0d5d50c395714d34b707d6f
Tentone <tentone@outlook.com>
2019-03-15 16:21:55 +0000

    Derp

--- build/potree.module.js
+++ build/potree.module.js
@@ -1,42 +1,166 @@
 /*
-<script src="lib/BinaryHeap.js"></script>
-
-<script src="source/utils/LRU.js"></script>
-<script src="source/utils/Enum.js"></script>
-<script src="source/utils/HelperUtils.js"></script>
-<script src="source/utils/VersionUtils.js"></script>
-<script src="source/utils/WorkerManager.js"></script>
-
-<script src="source/Potree.js"></script>
-
-<script src="source/PointAttributes.js"></script>
-<script src="source/Gradients.js"></script>
-<script src="source/Points.js"></script>
-<script src="source/Shader.js"></script>
-<script src="source/WebGLTexture.js"></script>
-<script src="source/WebGLBuffer.js"></script>
-<script src="source/Shaders.js"></script>
-
-<script src="source/pointcloud/DEM.js"></script>
-<script src="source/pointcloud/DEMNode.js"></script>
-<script src="source/pointcloud/PointCloudTree.js"></script>
-<script src="source/pointcloud/PointCloudArena4D.js"></script>
-<script src="source/pointcloud/PointCloudOctree.js"></script>
-
-<script src="source/pointcloud/geometries/PointCloudOctreeGeometry.js"></script>
-<script src="source/pointcloud/geometries/PointCloudArena4DGeometry.js"></script>
-<script src="source/pointcloud/geometries/PointCloudGreyhoundGeometry.js"></script>
-
-<script src="source/pointcloud/materials/PointCloudMaterial.js"></script>
-
-<script src="source/loaders/LASLoader.js"></script>
-<script src="source/loaders/BinaryLoader.js"></script>
-<script src="source/loaders/GreyhoundUtils.js"></script>
-<script src="source/loaders/GreyhoundLoader.js"></script>
-<script src="source/loaders/GreyhoundBinaryLoader.js"></script>
-<script src="source/loaders/POCLoader.js"></script>
-<script src="source/loaders/LASLAZLoader.js"></script>
-
-<script src="source/objects/BasicGroup.js"></script>
-<script src="source/objects/Group.js"></script>
+** Binary Heap implementation in Javascript
+** From: http://eloquentjavascript.net/1st_edition/appendix2.htmlt
+**
+** Copyright (c) 2007 Marijn Haverbeke, last modified on November 28 2013.
+**
+** Licensed under a Creative Commons attribution-noncommercial license. 
+** All code in this book may also be considered licensed under an MIT license.
 */
+
+
+
+function BinaryHeap(scoreFunction){
+  this.content = [];
+  this.scoreFunction = scoreFunction;
+}
+
+BinaryHeap.prototype = {
+  push: function(element) {
+    // Add the new element to the end of the array.
+    this.content.push(element);
+    // Allow it to bubble up.
+    this.bubbleUp(this.content.length - 1);
+  },
+
+  pop: function() {
+    // Store the first element so we can return it later.
+    var result = this.content[0];
+    // Get the element at the end of the array.
+    var end = this.content.pop();
+    // If there are any elements left, put the end element at the
+    // start, and let it sink down.
+    if (this.content.length > 0) {
+      this.content[0] = end;
+      this.sinkDown(0);
+    }
+    return result;
+  },
+
+  remove: function(node) {
+    var length = this.content.length;
+    // To remove a value, we must search through the array to find
+    // it.
+    for (var i = 0; i < length; i++) {
+      if (this.content[i] != node) continue;
+      // When it is found, the process seen in 'pop' is repeated
+      // to fill up the hole.
+      var end = this.content.pop();
+      // If the element we popped was the one we needed to remove,
+      // we're done.
+      if (i == length - 1) break;
+      // Otherwise, we replace the removed element with the popped
+      // one, and allow it to float up or sink down as appropriate.
+      this.content[i] = end;
+      this.bubbleUp(i);
+      this.sinkDown(i);
+      break;
+    }
+  },
+
+  size: function() {
+    return this.content.length;
+  },
+
+  bubbleUp: function(n) {
+    // Fetch the element that has to be moved.
+    var element = this.content[n], score = this.scoreFunction(element);
+    // When at 0, an element can not go up any further.
+    while (n > 0) {
+      // Compute the parent element's index, and fetch it.
+      var parentN = Math.floor((n + 1) / 2) - 1,
+      parent = this.content[parentN];
+      // If the parent has a lesser score, things are in order and we
+      // are done.
+      if (score >= this.scoreFunction(parent))
+        break;
+
+      // Otherwise, swap the parent with the current element and
+      // continue.
+      this.content[parentN] = element;
+      this.content[n] = parent;
+      n = parentN;
+    }
+  },
+
+  sinkDown: function(n) {
+    // Look up the target element and its score.
+    var length = this.content.length,
+    element = this.content[n],
+    elemScore = this.scoreFunction(element);
+
+    while(true) {
+      // Compute the indices of the child elements.
+      var child2N = (n + 1) * 2, child1N = child2N - 1;
+      // This is used to store the new position of the element,
+      // if any.
+      var swap = null;
+      // If the first child exists (is inside the array)...
+      if (child1N < length) {
+        // Look it up and compute its score.
+        var child1 = this.content[child1N],
+        child1Score = this.scoreFunction(child1);
+        // If the score is less than our element's, we need to swap.
+        if (child1Score < elemScore)
+          swap = child1N;
+      }
+      // Do the same checks for the other child.
+      if (child2N < length) {
+        var child2 = this.content[child2N],
+        child2Score = this.scoreFunction(child2);
+        if (child2Score < (swap == null ? elemScore : child1Score))
+          swap = child2N;
+      }
+
+      // No need to swap further, we are done.
+      if (swap == null) break;
+
+      // Otherwise, swap and continue.
+      this.content[n] = this.content[swap];
+      this.content[swap] = element;
+      n = swap;
+    }
+  }
+};
+
+/*
+import * from "./utils/LRU.js";
+import * from "./utils/Enum.js";
+import * from "./utils/HelperUtils.js";
+import * from "./utils/VersionUtils.js";
+import * from "./utils/WorkerManager.js";
+
+import * from "./Potree.js";
+import * from "./PointAttributes.js";
+import * from "./Gradients.js";
+import * from "./Points.js";
+import * from "./Shader.js";
+import * from "./WebGLTexture.js";
+import * from "./WebGLBuffer.js";
+import * from "./Shaders.js";
+
+import * from "./pointcloud/DEM.js";
+import * from "./pointcloud/DEMNode.js";
+import * from "./pointcloud/PointCloudTree.js";
+import * from "./pointcloud/PointCloudArena4D.js";
+import * from "./pointcloud/PointCloudOctree.js";
+
+import * from "./pointcloud/geometries/PointCloudOctreeGeometry.js";
+import * from "./pointcloud/geometries/PointCloudArena4DGeometry.js";
+import * from "./pointcloud/geometries/PointCloudGreyhoundGeometry.js";
+
+import * from "./pointcloud/materials/PointCloudMaterial.js";
+
+import * from "./loaders/LASLoader.js";
+import * from "./loaders/BinaryLoader.js";
+import * from "./loaders/GreyhoundUtils.js";
+import * from "./loaders/GreyhoundLoader.js";
+import * from "./loaders/GreyhoundBinaryLoader.js";
+import * from "./loaders/POCLoader.js";
+import * from "./loaders/LASLAZLoader.js";
+
+import * from "./objects/BasicGroup.js";
+import * from "./objects/Group.js";
+*/
+
+export { BinaryHeap };
--- source/Main.js
+++ source/Main.js
@@ -1,45 +1,40 @@
 "use strict";
 
+export {BinaryHeap} from "./lib/BinaryHeap.js";
 
-/*
-<script src="lib/BinaryHeap.js"></script>
+export {LRU} from "./utils/LRU.js";
+export {HelperUtils} from "./utils/HelperUtils.js";
+export {VersionUtils} from "./utils/VersionUtils.js";
+export {WorkerManager} from "./utils/WorkerManager.js";
 
-<script src="source/utils/LRU.js"></script>
-<script src="source/utils/Enum.js"></script>
-<script src="source/utils/HelperUtils.js"></script>
-<script src="source/utils/VersionUtils.js"></script>
-<script src="source/utils/WorkerManager.js"></script>
+export {Potree} from "./Potree.js";
+export {PointAttribute, PointAttributes, PointAttributeNames, PointAttributeTypes} from "./PointAttributes.js";
+export {Gradients} from "./Gradients.js";
+export {Points} from "./Points.js";
+export {Shader} from "./Shader.js";
+export {WebGLTexture} from "./WebGLTexture.js";
+export {WebGLBuffer} from "./WebGLBuffer.js";
+export {Shaders} from "./Shaders.js";
 
-<script src="source/Potree.js"></script>
+export {DEM} from "./pointcloud/DEM.js";
+export {DEMNode} from "./pointcloud/DEMNode.js";
+export {PointCloudTree} from "./pointcloud/PointCloudTree.js";
+export {PointCloudArena4D} from "./pointcloud/PointCloudArena4D.js";
+export {PointCloudOctree} from "./pointcloud/PointCloudOctree.js";
 
-<script src="source/PointAttributes.js"></script>
-<script src="source/Gradients.js"></script>
-<script src="source/Points.js"></script>
-<script src="source/Shader.js"></script>
-<script src="source/WebGLTexture.js"></script>
-<script src="source/WebGLBuffer.js"></script>
-<script src="source/Shaders.js"></script>
+export {PointCloudOctreeGeometry} from "./pointcloud/geometries/PointCloudOctreeGeometry.js";
+export {PointCloudArena4DGeometry} from "./pointcloud/geometries/PointCloudArena4DGeometry.js";
+export {PointCloudGreyhoundGeometry} from "./pointcloud/geometries/PointCloudGreyhoundGeometry.js";
 
-<script src="source/pointcloud/DEM.js"></script>
-<script src="source/pointcloud/DEMNode.js"></script>
-<script src="source/pointcloud/PointCloudTree.js"></script>
-<script src="source/pointcloud/PointCloudArena4D.js"></script>
-<script src="source/pointcloud/PointCloudOctree.js"></script>
+export {PointCloudMaterial} from "./pointcloud/materials/PointCloudMaterial.js";
 
-<script src="source/pointcloud/geometries/PointCloudOctreeGeometry.js"></script>
-<script src="source/pointcloud/geometries/PointCloudArena4DGeometry.js"></script>
-<script src="source/pointcloud/geometries/PointCloudGreyhoundGeometry.js"></script>
+export {LASLoader} from "./loaders/LASLoader.js";
+export {BinaryLoader} from "./loaders/BinaryLoader.js";
+export {GreyhoundUtils} from "./loaders/GreyhoundUtils.js";
+export {GreyhoundLoader} from "./loaders/GreyhoundLoader.js";
+export {GreyhoundBinaryLoader} from "./loaders/GreyhoundBinaryLoader.js";
+export {POCLoader} from "./loaders/POCLoader.js";
+export {LASLAZLoader} from "./loaders/LASLAZLoader.js";
 
-<script src="source/pointcloud/materials/PointCloudMaterial.js"></script>
-
-<script src="source/loaders/LASLoader.js"></script>
-<script src="source/loaders/BinaryLoader.js"></script>
-<script src="source/loaders/GreyhoundUtils.js"></script>
-<script src="source/loaders/GreyhoundLoader.js"></script>
-<script src="source/loaders/GreyhoundBinaryLoader.js"></script>
-<script src="source/loaders/POCLoader.js"></script>
-<script src="source/loaders/LASLAZLoader.js"></script>
-
-<script src="source/objects/BasicGroup.js"></script>
-<script src="source/objects/Group.js"></script>
-*/
+export {BasicGroup} from "./objects/BasicGroup.js";
+export {Group} from "./objects/Group.js";

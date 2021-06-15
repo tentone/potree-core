89dd7cef78dacc22fc2d736ba331110011a914fa
Maksim Nesterenko <maksim.nesterenko@cognite.com>
2021-06-15 17:54:10 +0200

    fix(WorkerManager): throw when unknown worker is requested

--- source/utils/WorkerManager.js
+++ source/utils/WorkerManager.js
@@ -12,26 +12,21 @@ import EptBinaryDecoderWorker from "../workers/EptBinaryDecoderWorker";
 /**
  * The worker manager is responsible for creating and managing worker instances.
  */
-class WorkerManager
-{
-	constructor()
-	{
-		this.workers = [];
+class WorkerManager {
+  constructor() {
+    this.workers = [];
 
-		for(var i = 0; i < 7; i++)
-		{
-			this.workers.push([]);
-		}
-	}
+    for (var i = 0; i < 7; i++) {
+      this.workers.push([]);
+    }
+  }
 
-	/**
-	 * Get a worker from the pool, if none available one will be created.
-	 */
-	getWorker(type)
-	{
-		if(this.workers[type].length > 0)
-		{
-			return this.workers[type].pop();
+  /**
+   * Get a worker from the pool, if none available one will be created.
+   */
+  getWorker(type) {
+    if (this.workers[type].length > 0) {
+      return this.workers[type].pop();
     }
 
     switch (type) {
@@ -49,43 +44,37 @@ class WorkerManager
         return new EptLaszipDecoderWorker();
       case 6:
         return new EptBinaryDecoderWorker();
-      defautl:
+      default:
         throw "Unknown worker requested";
+    }
+  }
+
+  /**
+   * Return (reinsert) the worker into the pool.
+   */
+  returnWorker(type, worker) {
+    this.workers[type].push(worker);
+  }
+
+  /**
+   * Run a task immediatly.
+   */
+  runTask(type, onMessage, message, transfer) {
+    var self = this;
+
+    var worker = this.getWorker(type);
+    worker.onmessage = function (event) {
+      onMessage(event);
+      self.returnWorker(type, worker);
     };
-	}
 
-	/**
-	 * Return (reinsert) the worker into the pool.
-	 */
-	returnWorker(type, worker)
-	{
-		this.workers[type].push(worker);
-	}
-
-	/**
-	 * Run a task immediatly.
-	 */
-	runTask(type, onMessage, message, transfer)
-	{
-		var self = this;
-
-		var worker = this.getWorker(type);
-		worker.onmessage = function(event)
-		{
-			onMessage(event);
-			self.returnWorker(type, worker);
-		};
-
-		if(transfer !== undefined)
-		{
-			worker.postMessage(message, transfer);
-		}
-		else
-		{
-			worker.postMessage(message);
-		}
-	}
-};
+    if (transfer !== undefined) {
+      worker.postMessage(message, transfer);
+    } else {
+      worker.postMessage(message);
+    }
+  }
+}
 
 WorkerManager.BINARY_DECODER = 0;
 WorkerManager.LAS_LAZ = 1;
@@ -95,4 +84,4 @@ WorkerManager.DEM = 4;
 WorkerManager.EPT_LAS_ZIP_DECODER = 5;
 WorkerManager.EPT_BINARY_DECODER = 6;
 
-export {WorkerManager};
+export { WorkerManager };

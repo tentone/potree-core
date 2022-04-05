d40a7c0a6380f9bd35fb79636e9b73dded21529c
tentone <tentone@outlook.com>
2022-04-05 23:05:19 +0100

    Derp

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,8 @@
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 
-function resolve(name) {
-  return path.resolve(__dirname, name);
-}
-
-/*
- * Set args on the command line using
- * ```bash
- * webpack --env.argname=value
- * ```
- */
-function getEnvArg(env, name, defaultValue) {
-  if (env === undefined) {
-    return defaultValue;
-  }
-  if (env[name] === undefined) {
-    return defaultValue;
-  }
-  if (env[name] === "true") {
-    return true;
-  }
-  if (env[name] === "false") {
-    return false;
-  }
-  return typeof env[name] === "string" ? env[name].trim() : env[name];
-}
-
-const isDevelopment = getEnvArg(process.env, 'development', false);
-
 module.exports = {
-  mode: isDevelopment ? 'development' : 'production',
+  mode: 'development', //: 'production',
   entry: "./source/Main.js",
   externals: [nodeExternals()],
   module: {
@@ -38,7 +10,7 @@ module.exports = {
       {
         test: /Worker\.js$/,
         loader: 'worker-loader',
-        include: [resolve('source')],
+        include: [path.resolve(__dirname,'source')],
         exclude: /node_modules/,
         options: { inline: true, fallback: false },
       },
@@ -53,5 +25,5 @@ module.exports = {
     sourceMapFilename: '[name].map',
     libraryTarget: 'commonjs',
   },
-  devtool: isDevelopment ? 'inline-source-map' : 'source-map'
+  devtool: 'inline-source-map' // : 'source-map'
 };
